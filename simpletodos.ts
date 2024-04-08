f437cacd6f4c2899ce7907b2cb7321d4afed105e
AbeeraKhalid <khalidabira19@gmail.com>
2024-04-08 08:12:38 +0500

    edit simpletodos.ts

--- simpletodos.ts
+++ simpletodos.ts
@@ -3,9 +3,8 @@ import inquirer from"inquirer";
 import chalk from "chalk";
 
 let Todos=[]
-let state = true; //condition
-while(state)
-    {
+// let state = true; //condition
+while(true){
     let To_dostask= await inquirer.prompt([
         {
         name:"todo_ques",
@@ -13,17 +12,29 @@ while(state)
         message:"What would you like to add in your Todos list?"
     },
     {
-        name:"addmore_task/2ndques",
-        type:"confirm",
+        name:"addmore_task",
+        type:'list',
+        choices:["yes","no"],
         message:"Do you want to add more tasks in your todos list?",
-        default:"true"
+    
 
     }
 
 ]);
+const{todo_ques,addmore_task}= To_dostask;
 Todos.push(To_dostask.todo_ques);
-console.log(chalk.bgMagentaBright.yellow(Todos));
-state= To_dostask.addmore_task;
+if(addmore_task=== "no"){
+    console.log(chalk.bgGreen.italic("My Todos list:"));
+    for(let i=0; i<=Todos.length; i++){
+        console.log(Todos[i]);
+
+}
+break;
+}
+
+// console.log(chalk.bgMagentaBright.yellow(Todos));
+// state= To_dostask.addmore_task;
+
 
 
 }
